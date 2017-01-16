4298396b4d162d862ee9bc30edd8315ab490ada2
imWillX <wxu13@student.mtsac.edu>
2017-01-15 23:53:12 -0800

    Update Expense Form

--- model/ExpenseForm.js
+++ model/ExpenseForm.js
@@ -1,12 +1,23 @@
 var mongoose = require('mongoose')
 var schema = mongoose.Schema()
 
+/**
+ * Notes:
+ * Calendar UI for dates.
+ * Department enums
+ * Category enums
+ */
+
 var FormSchema = mongoose.Schema({
     first_name: String,
   	last_name: String,
     email: String,
-    cost: String,
-    link: String
+    date: Date,
+    purchase_date: Date,
+    item_description: String,
+    supplier: String,
+    department: String,
+    category: String
 })
 
 module.exports = mongoose.model('Form', FormSchema)
