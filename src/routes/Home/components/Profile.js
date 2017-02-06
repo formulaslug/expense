f936628c67f59a803bafe31ba7a1dfbc0066bcfb
Alex Price <alex@alexprice.co>
2017-02-05 20:57:16 -0800

    Set up state

--- src/routes/Home/components/Profile.js
+++ src/routes/Home/components/Profile.js
@@ -7,18 +7,12 @@ class Profile extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      convo: {},
-      user: {
-        name: {
-          first: 'Alex',
-          last: 'Price'
-        }
-      },
-      nextSession: {
-        startTime: '12:00',
-        chargerGroup: 'Faraday Building',
-        charger: '4'
-      }
+      legalName: null,
+      ucscEmail: null,
+      phone: null,
+      address: null,
+      ssn: null,
+      payeeSetupSubmitted: false
     }
   }
 
@@ -65,28 +59,29 @@ class Profile extends Component {
           <div className='row'>
             <div className='key'>Saved data</div>
             <div className='key'>Edit</div>
+            {/* TODO: Make that edit button do stuff */}
           </div>
           <div className='row'>
             <div className='key'>Legal name</div>
-            <div className='value'>&#60;name&#62;</div>
+            <div className='value'>{ this.state.legalName }</div>
           </div>
           <div className='row'>
             <div className='key'>UCSC email</div>
-            <div className='value'>&#60;email&#62;</div>
+            <div className='value'>{ this.state.ucscEmail }</div>
           </div>
           <div className='row'>
             <div className='key'>Phone</div>
-            <div className='value'>&#60;phone&#62;</div>
+            <div className='value'>{ this.state.phone }</div>
           </div>
           <div className='row'>
             <div className='key'>Address</div>
-            <div className='value'>&#60;address&#62;</div>
+            <div className='value'>{ this.state.address }</div>
           </div>
           <div className='row'>
             <div className='key'>SSN</div>
-            <div className='value'>&#60;ssn&#62;</div>
+            <div className='value'>{ this.state.ssn }</div>
           </div>
-          { this.payeeSetupSubmitted(false) }
+          { this.payeeSetupSubmitted(this.state.payeeSetupSubmitted) }
         </div>
 
         <Link to='/expense' className='button'>File a new expense report</Link>
